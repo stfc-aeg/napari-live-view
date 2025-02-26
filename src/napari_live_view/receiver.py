50b6e6d26dea3eb6b07bbcff09c75fd31dd93cb0
Evelyn Johns <evelyn.johns@stfc.ac.uk>
2025-02-26 14:47:13 +0000

    Initial commit

---
+++ src/napari_live_view/receiver.py
@@ -0,0 +1,97 @@
+import zmq
+import time
+import numpy as np
+from napari.qt.threading import thread_worker
+
+class Receiver():
+    """Receiver class.
+    
+    This class controls the napari thread worker that receives and formats
+    data from ZMQ.
+    """
+
+    def __init__(self):
+        """Initialise receiver class."""
+        self.endpoint_url = ""
+        self.frames_received = 0
+        self.connected = False
+
+    def connect(self, endpoint_url):
+        """Creates ZMQ context and socket, connects to endpoint URL given."""
+        try:
+            self.endpoint_url = endpoint_url
+            self.context = zmq.Context()
+            self.socket = self.context.socket(zmq.SUB)
+            self.socket.setsockopt_string(zmq.SUBSCRIBE, "")
+            self.socket.connect(self.endpoint_url)
+            self.connected = True
+        except:
+            print("Invalid url")
+    
+    def disconnect(self):
+        """Disconnects from ZMQ socket and destroys context."""
+        self.socket.disconnect(self.endpoint_url)
+        self.context.destroy()
+        self.connected = False
+
+    def start_worker(self):
+        """Creates worker from @thread_worker decorated function, starts worker."""
+        self.worker = self.yield_frames()
+        self.worker.start()
+
+    def connect_worker(self, func):
+        """Connects worker to given function."""
+        self.worker.yielded.connect(func)
+
+    # combine start_worker and connect_worker?
+
+    def stop_worker(self):
+        """Stops thread worker and waits until aborted to call disconnect."""
+        self.worker.quit()
+        aborted = False
+        while not aborted:
+            if self.worker.aborted:
+                self.disconnect()
+                aborted = True
+                print("Disconnected")
+
+    @thread_worker
+    def yield_frames(self):
+        """Receives data from socket.
+        
+        @thread_worker decorated function.
+        """
+        while self.connected:
+
+            header = {}
+            frame_data = None
+            buf = None
+
+            flags = self.socket.getsockopt(zmq.EVENTS)
+            # Get socket event flags
+
+            # Keep handling socket events until all flags are cleared
+            while flags != 0:
+                
+                if flags & zmq.POLLIN:
+
+                    # Receive frame header
+                    header = self.socket.recv_json()
+
+                    # Receive data payload
+                    msg = self.socket.recv()
+                    buf = memoryview(msg)
+
+                    # Increment frames received counter
+                    self.frames_received += 1
+
+                flags = self.socket.getsockopt(zmq.EVENTS) 
+
+            # Convert last received payload into numpy array and reshape
+            if buf is not None:
+                array = np.frombuffer(buf, dtype=header['dtype'])
+                frame_data = array.reshape([int(header["shape"][0]), int(header["shape"][1])])
+
+            yield frame_data
+            
+            time.sleep(0.1)
---
+++ src/napari_live_view/viewer.py
@@ -0,0 +1,159 @@
+import json
+import os
+from qtpy.QtWidgets import QPushButton, QWidget, QLabel, QLineEdit, QGridLayout, QSizePolicy, QVBoxLayout
+from .receiver import Receiver
+
+class Viewer(QWidget):
+    """"Viewer class.
+    
+    This class connects the Receiver to napari layers, and adds dock widgets.
+    """
+
+    def __init__(self, viewer: 'napari.viewer.Viewer'):
+        super().__init__()
+        """Initialise Viewer class."""       
+
+        self.viewer = viewer
+        self.receiver = Receiver()
+
+        self.layout = QVBoxLayout()
+        self.setSizePolicy(QSizePolicy.Policy.Preferred, QSizePolicy.Policy.Maximum)
+        self.setLayout(self.layout)
+
+        self.frames_shown = 0
+
+        self.add_connections_widget()
+        self.add_counter_widget()
+        self.get_defaults()
+        
+    def get_defaults(self):
+        """Gets default endpoint from config file. 
+        
+        Creates default file if it doesn't already exist.
+        """
+        home = os.environ['HOME']
+        path = home + "/.config/napari/live_view_default.json"
+
+        while not os.access(path, os.F_OK):
+            print("Defaults file created") 
+            with open(os.path.join((home + "/.config/napari"), "live_view_default.json"), 'w') as f:
+                defaults = {
+                    "default_endpoint": ""
+                }
+                json.dump(defaults, f)
+
+        with open(path) as f:
+            default = json.load(f)
+            endpoint = default["default_endpoint"]
+            if endpoint != "": # better way to do this? - additional field T/F if a defaults being used?
+                self.endpoint_input.insert(endpoint)
+                self.button_clicked()
+
+    def hideEvent(self, event):
+        """Overwrites QWidget event to close ZMQ connection when widget is hidden."""
+        self.disconnect()
+
+    def update_layer(self, img):
+        """Updates or adds layer in napari with given image."""
+        if img is not None:
+            try:
+                self.viewer.layers['result'].data = img
+            except KeyError:
+                    self.viewer.add_image(
+                        img, name='result'
+                    )
+            self.frames_shown += 1
+            self.update_counters()
+
+    def update_counters(self):
+        """Repaints frames shown and received counters in dock widget."""
+        self.frames_shown_counter.setText(str(self.frames_shown))
+        self.frames_shown_counter.repaint()
+        self.frames_recvd_counter.setText(str(self.receiver.frames_received))
+        self.frames_recvd_counter.repaint()
+
+    def reset_counters(self):
+        """Resets frames shown and received counters to zero and updates widget
+        display.
+        """
+        self.frames_shown = 0
+        self.receiver.frames_received = 0
+        self.update_counters()
+
+    def add_connections_widget(self):
+        """Adds endpoint URL text input and connect button widgets."""
+        connections = GridWidget()
+        self.layout.addWidget(connections)
+
+        self.endpoint_input = QLineEdit()
+        self.endpoint_input.setPlaceholderText("Endpoint URL")
+        self.connect_button = QPushButton("CONNECT")
+        self.connect_button.clicked.connect(self.button_clicked)
+
+        connections.add(self.endpoint_input, 1, 1)
+        connections.add(self.connect_button, 2, 1)
+
+    def add_counter_widget(self):
+        """Adds counter widgets with labels, and reset button."""
+        counter = GridWidget()
+        self.layout.addWidget(counter)
+
+        frames_shown_label = QLabel("Frames shown: ")
+        frames_recvd_label = QLabel("Frames received: ")
+
+        self.frames_shown_counter = QLabel(str(self.frames_shown))
+        self.frames_recvd_counter = QLabel(str(self.receiver.frames_received))
+        reset_button = QPushButton("Reset counters")
+        reset_button.clicked.connect(self.reset_counters)
+
+        counter.add(self.frames_shown_counter, 1, 2)
+        counter.add(frames_shown_label, 1, 1)
+        counter.add(self.frames_recvd_counter, 2, 2)
+        counter.add(frames_recvd_label, 2, 1)
+        counter.add(reset_button, 3, 1, 1, 2)
+
+    def button_clicked(self):
+        """Called when the connections button is clicked.
+        
+        Depending on buttons state, calls either connect or disconnect, and
+        updates appearance.
+        """
+        if self.connect_button.text() == "CONNECT":
+            self.connect_button.setText("DISCONNECT")
+            self.endpoint_input.setReadOnly(True) 
+            # could have the input detect if text is changed and automatically disconnect and reconnect?
+            self.endpoint_url = self.endpoint_input.text()
+            self.connect(self.endpoint_url)
+        else:
+            self.connect_button.setText("CONNECT")
+            self.endpoint_input.setReadOnly(False)
+            self.disconnect()
+
+    def connect(self, endpoint_url):
+        """Connects given endpoint URL to receiver, starts and connects worker."""
+        self.receiver.connect(endpoint_url)
+        self.receiver.start_worker()
+        self.receiver.connect_worker(self.update_layer)
+        if self.receiver.connected:
+            print("Connected") # move to receiver?
+
+    def disconnect(self):
+        """Stops worker."""
+        if self.receiver.connected:
+            self.receiver.stop_worker()
+
+class GridWidget(QWidget):
+    """GridWidget class.
+    
+    This class inherits from QWidget and creates a grid layout."""
+    def __init__(self):
+        super().__init__()
+        """Initialise GridWidget class."""
+
+        self.layout = QGridLayout()
+        self.setSizePolicy(QSizePolicy.Policy.Preferred, QSizePolicy.Policy.Maximum)
+        self.setLayout(self.layout)
+
+    def add(self, widget, row, col, rowspan=1, colspan=1):
+        """Adds given widget to the grid at row/col given."""
+        self.layout.addWidget(widget, row, col, rowspan, colspan)
